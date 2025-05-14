de67bfa512ad28f41cb79ff559462de909f16552
Zhivko Shopov <zhishopov@gmail.com>
2025-05-14 17:04:17 +0100

    Refactor day of week

--- intro-typescript/dayOfWeek.ts
+++ intro-typescript/dayOfWeek.ts
@@ -1,31 +1,15 @@
-enum DaysOfWeek {
-  Monday = 1,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday,
-}
-
 function dayOfWeek(day: number) {
-  if (day === DaysOfWeek.Monday) {
-    console.log("Monday");
-  } else if (day === DaysOfWeek.Tuesday) {
-    console.log("Tuesday");
-  } else if (day === DaysOfWeek.Wednesday) {
-    console.log("Wednesday");
-  } else if (day === DaysOfWeek.Thursday) {
-    console.log("Thursday");
-  } else if (day === DaysOfWeek.Friday) {
-    console.log("Friday");
-  } else if (day === DaysOfWeek.Saturday) {
-    console.log("Saturday");
-  } else if (day === DaysOfWeek.Sunday) {
-    console.log("Sunday");
-  } else {
-    console.log("Error");
+  enum DaysOfWeek {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
   }
+
+  console.log(DaysOfWeek[day] || "error");
 }
 
-dayOfWeek(7);
+dayOfWeek(9);
