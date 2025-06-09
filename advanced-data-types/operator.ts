9b3af3f7580255d09b9410f114a40e0e562dfb1a
Zhivko Shopov <zhishopov@gmail.com>
2025-06-09 10:20:24 +0100

    Hotel Visit Solution

--- advanced-data-types/operator.ts
+++ advanced-data-types/operator.ts
@@ -18,3 +18,7 @@ function operator(
 
 console.log(operator(["First", "Second", "Third"], "Index", 1));
 console.log(operator("string", "Index", 1));
+console.log(operator(["Just", "Two"], "Length", 5));
+console.log(operator("short string1", "Length", 5));
+console.log(operator("7", "Add", 3));
+console.log(operator(11, "Add", 3));
---
+++ hotelVisit.ts
@@ -0,0 +1,53 @@
+type gym = { number: 1; train: () => void } & (
+  | { hallway: "A"; pass?: "Guest" }
+  | { hallway: "C" }
+);
+
+type restaurant = { number: 2; dine: () => void } & (
+  | { hallway: "A"; pass?: "Guest" }
+  | { hallway: "C" }
+);
+
+type hotel = { number: 3; sleep: () => void; hallway: "A" | "C" };
+
+type simplified = gym | restaurant | hotel;
+
+function visitFloor(floor: simplified) {
+  switch (floor.number) {
+    case 1:
+      floor.train();
+      return;
+    case 2:
+      floor.dine();
+      return;
+    case 3:
+      floor.sleep();
+      return;
+  }
+}
+
+visitFloor({ train() {}, number: 1, hallway: "A", pass: "Guest" });
+
+visitFloor({ dine() {}, number: 2, hallway: "A" });
+
+visitFloor({ sleep() {}, number: 3, hallway: "C" });
+
+visitFloor({ train() {}, number: 1, hallway: "C" });
+
+visitFloor({ train() {}, number: 1, hallway: "A" });
+
+visitFloor({ dine() {}, number: 2, hallway: "A", pass: "Guest" });
+
+visitFloor({ sleep() {}, number: 3, hallway: "A" });
+visitFloor({ dine() {}, number: 2, hallway: "C" });
+
+// Test Error Input (should give TS error)
+// visitFloor({ train() { }, number: 4, hallway: 'A' });
+
+// visitFloor({ train() { }, number: 1, hallway: 'C', pass: 'Guest' });
+
+// visitFloor({ train() { }, number: 2, hallway: 'A' });
+
+// visitFloor({ train() { }, number: 3, hallway: 'C' });
+
+// visitFloor({ train() { }, number: 3, hallway: 'C', pass: 'Guest' });
